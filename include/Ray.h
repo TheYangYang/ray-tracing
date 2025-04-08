3b2ddba00c004868473368ba258d948e347044ef
TheYangYang <changgeng333@gmail.com>
2025-04-08 12:58:29 -0500

    chore: remove all member variables prefix m_

--- include/Ray.h
+++ include/Ray.h
@@ -11,12 +11,12 @@ public:
     Ray(const Ray &) = default;
     Ray &operator=(const Ray &) = default;
 
-    math::Vector3 GetDirection() const { return m_Direction; }
-    math::Vector3 GetOrigin() const { return m_Origin; }
+    math::Vector3 GetDirection() const { return direction; }
+    math::Vector3 GetOrigin() const { return origin; }
 
-    math::Vector3 At(float t) const { return m_Origin + t * m_Direction; }
+    math::Vector3 At(float t) const { return origin + t * direction; }
 
 private:
-    math::Vector3 m_Origin;
-    math::Vector3 m_Direction;
+    math::Vector3 origin;
+    math::Vector3 direction;
 };
--- src/Ray.cpp
+++ src/Ray.cpp
@@ -1,13 +1,13 @@
 #include "Ray.h"
 
 Ray::Ray()
-    : m_Origin(math::Vector3()), m_Direction(math::Vector3())
+    : origin(math::Vector3()), direction(math::Vector3())
 {
 
 }
 
 Ray::Ray(const math::Vector3& origin, const math::Vector3& direction)
-    :m_Origin(origin), m_Direction(direction)
+    :origin(origin), direction(direction)
 {
 
 }
